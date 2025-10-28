3d82b6c66f6ee4825033a049c48dff17a09c996b
Valerian <walerian813@gmail.com>
2025-10-28 13:52:22 +0200

    Craft
    Started adding craft system

--- Assets/Player/Scripts/CraftController.cs
+++ Assets/Player/Scripts/CraftController.cs
@@ -1,7 +1,38 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class CraftController : MonoBehaviour
 {
+    private InventoryController _inventoryController;
 
+    [System.Serializable]
+    public struct ItemBluePrint
+    {
+        public Button CraftItemButton;
+        public ItemData Item;
+
+        public int Woods;
+        public int Stones;
+    }
+
+    [SerializeField] private ItemBluePrint[] ItemsBluePrints;
+
+    private void Awake()
+    {
+        _inventoryController = GetComponent<InventoryController>();
+
+        for (int i = 0; i < ItemsBluePrints.Length; i++)
+        {
+            int index = i;
+            ItemsBluePrints[i].CraftItemButton.onClick.AddListener(() => CraftItem(index));
+        }
+    }
+
+    private void CraftItem(int index)
+    {
+        var item = ItemsBluePrints[index];
+        Debug.Log($"You clicked button #{index}");
+        //Debug.Log($"You clicked button #{index} for item {item.Item.name}");
+    }
 }
